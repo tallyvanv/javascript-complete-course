eebb53c71b426ebf3083e74199f0484aad76029a
tallyvanv <talisa.vdv@gmail.com>
2019-12-16 16:35:08 +0100

    yay my calculator works after adding a + to make clear the numbers aren't strings

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -1,19 +1,28 @@
+// to get the value of an input: document.getElementById("element-id").value (defined outside of function)
+
+let a = +document.getElementById("op-one").value;
+let b = +document.getElementById("op-two").value;
+
 (function() {
-    // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        alert(a+b);
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        alert(a-b);
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        alert(a*b);
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        alert(a/b);
     });
 })();
+
