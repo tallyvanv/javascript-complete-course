2e18ba813a08c60d22e9c160cd5a00aa78990300
tallyvanv <talisa.vdv@gmail.com>
2019-12-16 14:57:53 +0100

    created html and script

---
+++ 02-maths/01-calculator-one/script.js
@@ -0,0 +1,19 @@
+(function() {
+    // to get the value of an input: document.getElementById("element-id").value
+
+    document.getElementById("addition").addEventListener("click", function() {
+        // perform an addition
+    });
+
+    document.getElementById("substraction").addEventListener("click", function() {
+        // perform an substraction
+    });
+
+    document.getElementById("multiplication").addEventListener("click", function() {
+        // perform an multiplication
+    });
+
+    document.getElementById("division").addEventListener("click", function() {
+        // perform an division
+    });
+})();
