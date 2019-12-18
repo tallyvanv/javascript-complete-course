fca77b89ac5e410005f773a84cd147a9c979ab4e
tallyvanv <talisa.vdv@gmail.com>
2019-12-18 16:56:34 +0100

    got the random color generator to work

--- 03-colors/03-change-bcg-three/script.js
+++ 03-colors/03-change-bcg-three/script.js
@@ -11,6 +11,13 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function () {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        document.body.style.backgroundColor = color;
+    })
 
 })();
