91814472fa1506c108f1dcaece1936aee82c29c5
tallyvanv <talisa.vdv@gmail.com>
2019-12-19 09:50:18 +0100

    put the minutes in the mix on 4.2

--- 04-dates/02-title-by-hour-two/script.js
+++ 04-dates/02-title-by-hour-two/script.js
@@ -5,15 +5,18 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     let d = new Date();
-    //d.setHours(19); -> set the hour for testing purposes
+/*    d.setHours(19); //-> set the hour for testing purposes
+    d.setMinutes(30); //=> set the minutes for testing purposes*/
     let currentHour = d.getHours(); //will get the hours as an integer between 0-23
+    let currentMinutes = d.getMinutes(); //will het the minutes between 0-59
 
-    if (currentHour < 18) //If the time is before 18h
+    if (currentHour >= 17 && currentMinutes >= 30)
+        //If the time is after 17 and minutes bigger than 30 (i.e. it's after 17.30)
     {
-        document.getElementById("target").innerHTML = "<i>Hello</i>";
+        document.getElementById("target").innerHTML = "<i>Good evening</i>";
         //gets the inner html of div with id target and changes content
     } else {
-        document.getElementById("target").innerHTML = "<i>Good evening</i>";
+        document.getElementById("target").innerHTML = "<i>Hello</i>";
         //cf. supra
     }
 
