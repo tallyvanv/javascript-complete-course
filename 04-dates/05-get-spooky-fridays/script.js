d8ad911c55abcdecf6abef15f0ac573824432d2f
tallyvanv <talisa.vdv@gmail.com>
2019-12-19 12:30:23 +0100

    managed to get alerts with results working on 4.5, will work on getting it into one alert box

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -1,16 +1,18 @@
-(function() {
+(function () {
     document.getElementById("run").addEventListener("click", function () {
-        let _13Year = +document.getElementById("year").value;
-       function friday13 () {
-           for (let month = 0; month < 12; month++) {//goes over every month in given year
-               let d = new Date(_13Year, month, 13);//check the 13th of every month
-               let count = 0;
-               if (d.getDay() == 5) {//check if this 13 of the month is friday
-                   alert(month + 1);
-                   count++;
-               }
-               return count;
-           }
-       }
-    });
+        let ourYears = Number(document.getElementById("year").value);
+
+        function friday13(year) {
+            for (let month = 0; month < 12; month++) {//goes over every month in given year
+                let d = new Date(year, month, 13);//check the 13th of every month
+                if (d.getDay() == 5) {//check if this 13 of the month is friday
+                    let ourMonths = ["January", "February", "March", "April", "May", "June", "July", "August",
+                        "September", "October", "November", "December"];
+                    //convert numerical months to words
+                    alert(ourMonths[Number(month)]);//trigger a pop-up with results
+                }
+            }
+        }
+        console.log(friday13(ourYears));//calculate results based on input
+    })
 })();
