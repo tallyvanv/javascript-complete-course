da7fdfdedaaa53fbb990e1b26b987965b326a408
tallyvanv <talisa.vdv@gmail.com>
2019-12-19 17:00:49 +0100

    started creating script for 5.9

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -1,16 +1,12 @@
-/* becode/javascript
- *
- * /05-arrays/09-rand-array-stats/script.js - 5.9: tableau aléatoire & statistiques
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (function() {
+    //generate random array with random numbers (random = between 0-1 so * 100)
+    let myNumbers = [...Array(10)].map(() => Math.floor(Math.random() * 100));
+    //trigger on click
+    document.getElementById("run").addEventListener("click", function() {
+        //iterate over entire array
+        for (i=0; i < myNumbers.length; i++) {
 
-    // your code here
+        }
+    })
 
 })();
