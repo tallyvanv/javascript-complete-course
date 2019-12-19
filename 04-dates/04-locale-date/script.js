f57b3a52c414670b0377089251837a8729e865a0
tallyvanv <talisa.vdv@gmail.com>
2019-12-19 11:32:12 +0100

    added comments and refined 4.4. Still figuring out hour format

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -1,7 +1,9 @@
 (function (string) {
-    var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};
-    let today = new Date();
+    let options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};
+    //these options decide how each date component will be displayed
+    let today = new Date();//create date variable
 
     document.getElementById("target").innerHTML = today.toLocaleDateString("en-UK", options);
+    //convert date variable to string with the aforementioned options, in UK format
 
 })();
