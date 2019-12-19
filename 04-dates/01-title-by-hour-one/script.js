1c8dd18e3fb4cdac080a0b308b19c8f651a533ae
tallyvanv <talisa.vdv@gmail.com>
2019-12-19 09:15:06 +0100

    got 4.1 to change div according to time (comments to be refined)

--- 04-dates/01-title-by-hour-one/script.js
+++ 04-dates/01-title-by-hour-one/script.js
@@ -1,18 +1,17 @@
-/* becode/javascript
- *
- * /04-dates/01-title-by-hour-one/script.js - 4.1: texte en fonction de l'heure (1)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (function() {
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    // your code here
+    let d = new Date();
+    //d.setHours(19); -> set the hour for testing purposes
+    let currentHour = d.getHours(); //will get the hours as an integer between 0-23
+
+    if (currentHour < 18) //If the time is before 18h
+    {
+        document.getElementById("target").innerHTML = "<i>Hello</i>";
+    }
+    else {
+        document.getElementById("target").innerHTML = "<i>Good evening</i>";
+    }
 
 })();
