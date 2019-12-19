76d9b2135273e5d1e3bd11e2cb27038e1db4436b
tallyvanv <talisa.vdv@gmail.com>
2019-12-19 11:02:09 +0100

    working on 4.4, how do I convert???

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -1,18 +1,9 @@
-/* becode/javascript
- *
- * /04-dates/04-locale-date/script.js - 4.4: date textuelle
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (function() {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+    let today = new Date();
+    let todayDate = today.toLocaleDateString();
+    let todayFormatted = DateFormatter.format();
 
-    // your code here
+    document.getElementById("target").innerHTML = today.toLocaleDateString();
 
 })();
