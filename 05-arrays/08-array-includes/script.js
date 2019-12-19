dfeb26f83ba9800a64d39b2d10d2b3fa0904edb0
tallyvanv <talisa.vdv@gmail.com>
2019-12-19 16:40:42 +0100

    finished 5.8 and added comments

--- 05-arrays/08-array-includes/script.js
+++ 05-arrays/08-array-includes/script.js
@@ -1,14 +1,3 @@
-/* becode/javascript
- *
- * /05-arrays/08-array-includes/script.js - 5.8: présence dans un tableau
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (function() {
 
     var fruits = [
@@ -25,7 +14,12 @@
         "grapes",
         "cherry",
     ];
-
-    // your code here
-
+    document.getElementById("run").addEventListener("click", function() {
+        if (fruits.includes("apple")) {//check if array includes "apple" with .includes method
+            console.log("yes");//say yes if so
+        }
+        else {
+            console.log("no");//say no if not so
+        }
+    })
 })();
