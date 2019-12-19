2941b9dc128b0b4ca4a274b6cc9118d3f5991fe0
tallyvanv <talisa.vdv@gmail.com>
2019-12-19 14:16:34 +0100

    finished assignment 5.3 with comments

--- 05-arrays/03-walk-one/script.js
+++ 05-arrays/03-walk-one/script.js
@@ -1,31 +1,24 @@
-/* becode/javascript
- *
- * /05-arrays/01-get-element/script.js - 5.1: élément de tableau
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
-
-    var fruits = [
-        "apple",
-        "pear",
-        "raspberry",
-        "tomatos",
-        "kiwi",
-        "banana",
-        "orange",
-        "mandarin",
-        "durian",
-        "peache",
-        "grap",
-        "cherry",
-    ];
-
-    // your code here
+    document.getElementById("run").addEventListener("click", function() {
+        var fruits = [
+            "apple",
+            "pear",
+            "raspberry",
+            "tomatos",
+            "kiwi",
+            "banana",
+            "orange",
+            "mandarin",
+            "durian",
+            "peache",
+            "grap",
+            "cherry",
+        ];
+        let i;
+        for (i=0; i < fruits.length; i++) {//iterate over array using for loop
+            console.log(fruits[i]);//log results in console
+        }
+    });
 
 })();
