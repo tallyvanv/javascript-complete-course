9c442b9b0c08e2931a2f904134f7beb221690e31
tallyvanv <talisa.vdv@gmail.com>
2019-12-20 14:52:18 +0100

    finished 5.9

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -3,12 +3,18 @@
     let myNumbers = [...Array(10)].map(() => Math.floor(Math.random() * 100));
     //trigger on click
     document.getElementById("run").addEventListener("click", function() {
-        //iterate over entire array
+        //iterate over entire array (starting from 1 cause of the id's starting at n-1)
         let i;
         for (i=1; i <= myNumbers.length; i++) {
-            let cellId = 'n-' +i;
-            document.getElementById(cellId).innerHTML = myNumbers[i-1];
+            let cellId = 'n-' +i;//iterate over different id's n-1...
+            document.getElementById(cellId).innerHTML = myNumbers[i-1];//populate the cells with numbers
         }
-        const arrMax = myNumbers => Math.max(...arr);
+        document.getElementById("min").innerHTML = Math.min(...myNumbers);//smallest number
+        document.getElementById("max").innerHTML = Math.max(...myNumbers);//biggest number
+        function getSum(total, num) {
+            return total + Math.round(num);//formula for sum
+        }
+        document.getElementById("sum").innerHTML = myNumbers.reduce(getSum, 0);//populate html with sum
+        document.getElementById("average").innerHTML = (myNumbers.reduce(getSum, 0))/myNumbers.length;
     })
 })();
