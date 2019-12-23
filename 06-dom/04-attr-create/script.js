0c91bb14acbc9ed84ed70cb23b25418d13b1703d
tallyvanv <talisa.vdv@gmail.com>
2019-12-23 11:49:31 +0100

    finished 6.4 and 6.5 with comments

--- 06-dom/04-attr-create/script.js
+++ 06-dom/04-attr-create/script.js
@@ -1,16 +1,12 @@
-/* becode/javascript
- *
- * /06-dom/04-attr-create/script.js - 6.4: manipulation d'attributs & création d'élément
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (function() {
 
-    // your code here
+    let sauce = document.getElementById("source");
+    let dataImage = sauce.attributes.getNamedItem("data-image").value;
+    let image = document.createElement("img");
+    let pictoir = document.getElementById("target").appendChild(image);
+    let att = document.createAttribute("src");
+    att.value = dataImage;
+    pictoir.setAttributeNode(att);
+    sauce.parentNode.removeChild(sauce);
 
 })();
--- 06-dom/05-hover-image/script.js
+++ 06-dom/05-hover-image/script.js
@@ -11,6 +11,15 @@
 
 (function() {
 
-    // your code here
+    let img = document.getElementsByTagName("img")[0];//select image in question
+    let dataHover = img.attributes.getNamedItem("data-hover").value;//use variable for attribute value
+    let noHover = img.attributes.getNamedItem("src").value;
 
+    img.addEventListener("mouseover", function () {//trigger function on hover
+        img.setAttribute("src", dataHover);//set attribute to variable 1
+    });
+
+    img.addEventListener('mouseout', function () {//trigger function on hover
+        img.setAttribute("src", noHover);//set attribute back to variable 2
+    })
 })();
