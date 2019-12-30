c110691a5db168b3e9e78e76f5c21dc23907b935
tallyvanv <talisa.vdv@gmail.com>
2019-12-30 11:01:53 +0100

    finished 10

--- 06-dom/10-match-password-two/script.js
+++ 06-dom/10-match-password-two/script.js
@@ -9,8 +9,16 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
-
+    document.getElementById("run").addEventListener("click", function () {
+        let field1 = document.getElementById("pass-one").value;
+        let field2 = document.getElementById("pass-two").value;
+        if (field1 !== field2) {
+            document.querySelectorAll("input").forEach(password => password.setAttribute("class", "error"));
+        }
+        else {
+            document.querySelectorAll("input").forEach(password => password.classList.remove("error"));
+        }
+    })
 })();
