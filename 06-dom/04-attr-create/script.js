2fcddf99675af3fdbbae53be88d6b6033a1ab788
tallyvanv <talisa.vdv@gmail.com>
2020-01-02 09:23:15 +0100

    added comments to 6.4

--- 06-dom/04-attr-create/script.js
+++ 06-dom/04-attr-create/script.js
@@ -1,12 +1,12 @@
 (function() {
 
-    let sauce = document.getElementById("source");
-    let dataImage = sauce.attributes.getNamedItem("data-image").value;
-    let image = document.createElement("img");
-    let pictoir = document.getElementById("target").appendChild(image);
-    let att = document.createAttribute("src");
-    att.value = dataImage;
-    pictoir.setAttributeNode(att);
-    sauce.parentNode.removeChild(sauce);
+    let sauce = document.getElementById("source");//define the element that has the needed attribute
+    let dataImage = sauce.attributes.getNamedItem("data-image").value;//get the value of the attribute
+    let image = document.createElement("img");//create an image element
+    let pictoir = document.getElementById("target").appendChild(image);//add image element to target element
+    let att = document.createAttribute("src");//create attribute within img
+    att.value = dataImage;//assign the necessary value extracted from element with id source
+    pictoir.setAttributeNode(att);//add attribute to target element
+    sauce.parentNode.removeChild(sauce);//remove source element
 
 })();
