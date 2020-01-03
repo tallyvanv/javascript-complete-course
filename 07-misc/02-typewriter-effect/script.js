307392c294beba8b48a5dbc7762b75cbd0eadf18
tallyvanv <talisa.vdv@gmail.com>
2020-01-03 10:00:03 +0100

    skipped 7.1 and finished 7.2

--- 07-misc/02-typewriter-effect/script.js
+++ 07-misc/02-typewriter-effect/script.js
@@ -11,6 +11,19 @@
 
 (function() {
 
-    // your code here
+    let i = 0;
+    let txt = document.getElementById("target").innerHTML; //get the text
+    let speed = Math.round(Math.random() * 500); //random speed
 
+    document.getElementById("target").innerHTML="";//empty element to put in text
+
+    function typeWriter() {
+        if (i < txt.length) {//go over each character in text
+            document.getElementById("target").innerHTML += txt.charAt(i);//add each character at index
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+
+    typeWriter();//initiate function
 })();
