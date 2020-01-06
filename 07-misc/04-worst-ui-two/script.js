4ba5a85eb76d307aa7131e37d9a86d2e4ee35991
tallyvanv <talisa.vdv@gmail.com>
2020-01-06 09:41:03 +0100

    finished 7.4

--- 07-misc/04-worst-ui-two/script.js
+++ 07-misc/04-worst-ui-two/script.js
@@ -11,6 +11,39 @@
 
 (function() {
 
-    // your code here
+    const boton1 = document.getElementById("part-one");//define the buttons as constants
+    const boton2 = document.getElementById("part-two");
+    const boton3 = document.getElementById("part-three");
+    const boton4 = document.getElementById("part-four");
 
+    let phonenr = document.getElementById("target");
+
+    boton1.onclick = function() {
+        let output1 = (parseInt(boton1.innerText++));
+        phonenr.innerHTML = "0" + boton1.innerHTML + boton2.innerHTML + boton3.innerHTML + boton4.innerHTML;
+    };
+
+    boton2.onclick = function() {
+        let output2 = (parseInt(boton2.innerText++));
+        if (output2 < 9) {
+            boton2.innerHTML = "0" + boton2.innerText;
+        }
+        phonenr.innerHTML = "0" + boton1.innerHTML + boton2.innerHTML + boton3.innerHTML + boton4.innerHTML;
+    };
+
+    boton3.onclick = function() {
+        let output3 = (parseInt(boton3.innerText++));
+        if (output3 < 9) {
+            boton3.innerHTML = "0" + boton3.innerText;
+        }
+        phonenr.innerHTML = "0" + boton1.innerHTML + boton2.innerHTML + boton3.innerHTML + boton4.innerHTML;
+    };
+
+    boton4.onclick = function() {
+        let output4 = (parseInt(boton4.innerText++));
+        if (output4 < 9) {
+            boton4.innerHTML = "0" + boton4.innerText;
+        }
+        phonenr.innerHTML = "0" + boton1.innerHTML + boton2.innerHTML + boton3.innerHTML + boton4.innerHTML;
+    };
 })();
