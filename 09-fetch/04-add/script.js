1010e0f0015ad3e5e9441ed7429948f4f3a27319
tallyvanv <talisa.vdv@gmail.com>
2020-01-08 16:49:28 +0100

    finished 9.4

--- 09-fetch/04-add/script.js
+++ 09-fetch/04-add/script.js
@@ -10,5 +10,24 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    const path = "http://localhost:12345/_shared/api.json";
+    async function inputHero() {
+        let newHero = {};//empty hero object
+        const response = await fetch(path);
+        const data = await response.json();
+        console.log(data);
+
+        newHero ["id"] = data.heroes.length + 1;
+        newHero ["name"]  = document.getElementById("hero-name").value;
+        newHero ["alterEgo"]  = document.getElementById("hero-alter-ego").value;
+        newHero ["abilities"] = new Array();
+        let abilitiesArr = document.getElementById("hero-powers").value;
+
+        newHero.abilities.push(abilitiesArr);
+
+        data.heroes.push(newHero);
+        console.log(data.heroes);
+    }
+    document.getElementById("run").onclick = function() {inputHero()};//trigger async function on click
 })();
